cdc7d4bacf2a6cdaf9af9ebdd614f2d42e047d8f
Trevor DiMartino <Trevor DiMartino>
2016-04-01 11:50:40 -0600

    Added push function to make growing list easier.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //http://cglab.ca/~abeinges/blah/too-many-lists/book/
 
 #![allow(dead_code)]
-
 use std::rc::Rc;
 
 #[derive(Clone,Debug,PartialEq,Eq)]
@@ -20,10 +19,14 @@ pub struct Iter<'a, T:'a> {
   next: Option<&'a Cons<T>>,
 }
 
+pub fn push<T>(list: List<T>, item: T) -> List<T> {
+  List::Cons(Cons{hd: item, tl:Rc::new(list)})
+}
+
 // fold : S list -> T -> (T -> S -> T) -> T
 
 pub fn fold<T,S,F>(list: &List<S>, accum:T, f:F) -> T
-  where F:Fn(T,&S) -> T {
+  where F:Fn(T, &S) -> T {
     match *list {
       List::Nil => accum,
       List::Cons(ref cons) => {
@@ -34,11 +37,21 @@ pub fn fold<T,S,F>(list: &List<S>, accum:T, f:F) -> T
 }
 
 #[test]
-pub fn test() {
+pub fn test_sum() {
   let l = List::Nil;
   let l = List::Cons(Cons{hd:3, tl:Rc::new(l)});
   let l = List::Cons(Cons{hd:2, tl:Rc::new(l)});
   let l = List::Cons(Cons{hd:1, tl:Rc::new(l)});
   let sum = fold(&l, 0, |sum,elm|{ sum + elm });
   assert_eq!(sum, 6);    
+}
+
+#[test]
+pub fn test_push() {
+  let l = List::Nil;
+  let l = push(l, 3);
+  let l = push(l, 2);
+  let l = push(l, 1);
+  let sum = fold(&l, 0, |sum, elm|{ sum + elm });
+  assert_eq!(sum, 6);
 }
